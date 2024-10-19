6dee8520129ec1c47cfa7f771cbcadc81d5eb096
Lite-VPN <154042720+Lite-VPN@users.noreply.github.com>
2024-10-19 18:22:11 +0700

    Add files via upload

--- menu/menu.sh
+++ menu/menu.sh
@@ -95,7 +95,7 @@ UDPX="https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-c
 BIBlack='\033[1;90m'      # Black
 BIRed='\033[1;91m'        # Red
 BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
+BIYellow='\E[40;1;37m'     # Yellow
 BIBlue='\033[1;94m'       # Blue
 BIPurple='\033[1;95m'     # Purple
 BICyan='\033[1;96m'       # Cyan
