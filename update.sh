6679877dde5ab683ffdf80bddb5023ba0b864e79
Lite-VPN <154042720+Lite-VPN@users.noreply.github.com>
2024-10-19 19:36:35 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,153 +1,68 @@
 #!/bin/bash
 
+# Check if the script is run as root
 if [ "${EUID}" -ne 0 ]; then
-echo "You need to run this script as root"
-exit 1
+  echo "You need to run this script as root"
+  exit 1
 fi
+
+# Check if running on OpenVZ, which is not supported
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-echo "OpenVZ is not supported"
-exit 1
+  echo "OpenVZ is not supported"
+  exit 1
 fi
+
 echo ""
+
+# Fetch the current and latest version
 version=$(cat /home/ver)
-ver=$( curl https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/version )
-clear
-line=$(cat /etc/line)
-below=$(cat /etc/below)
-back_text=$(cat /etc/back)
-number=$(cat /etc/number)
-box=$(cat /etc/box)
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info1="${Green_font_prefix}($version)${Font_color_suffix}"
-Info2="${Green_font_prefix}(OLD VERSION)${Font_color_suffix}"
-Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
-version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/version | grep $version )
-if [ $version = $new_version ]; then
-sts="${Info2}"
+new_version=$(curl -s https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/version)
+
+if [ "$version" == "$new_version" ]; then
+  status="${Green_font_prefix}(OLD VERSION)${Font_color_suffix}"
 else
-sts="${Error}"
+  status="${Red_font_prefix}[Please Update to Version $new_version]${Font_color_suffix}"
 fi
+
 clear
-curl https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/version | grep $version
-sleep 1
-echo ""
-cd /usr/bin
-wget -O run-update "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/update.sh"
-chmod +x run-update
-echo ""
-clear
+echo "Current version: $version"
+echo "Status: $status"
+
+# Begin update process
 echo -e "\e[0;32mPlease Wait...!\e[0m"
 sleep 1
 clear
-echo ""
-echo -e "\e[0;32mMendownload Versi Script Started......!!!\e[0m"
-sleep 1
-cd /usr/bin
-wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/usernew.sh"
-wget -q -O /usr/bin/auto-reboot "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/auto-reboot.sh"
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/restart.sh"
-wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/tendang.sh"
-wget -q -O /usr/bin/clearcache "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/clearcache.sh"
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/running.sh"
-wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/speedtest_cli.py"
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-vless.sh"
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-vmess.sh"
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-trojan.sh"
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-ssh.sh"
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-backup.sh"
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu.sh"
-wget -q -O /usr/bin/menu1 "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/theme/menu1.sh"
-wget -q -O /usr/bin/menu2 "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/theme/menu2.sh"
-wget -q -O /usr/bin/menu3 "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/theme/menu3.sh"
-wget -q -O /usr/bin/menu4 "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/theme/menu4.sh"
-wget -q -O /usr/bin/menu5 "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/theme/menu5.sh"
-wget -q -O /usr/bin/menu-webmin "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-webmin.sh"
-wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/xp.sh"
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/update.sh"
-wget -q -O /usr/bin/add-host "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/add-host.sh"
-wget -q -O /usr/bin/certv2ray "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/certv2ray.sh"
-wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-set.sh"
-wget -q -O /usr/bin/about "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/about.sh"
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-backup.sh"
-wget -q -O /usr/bin/trial "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/trial.sh"
-wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/ssh/usernew.sh"
-wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/add-tr.sh"
-wget -q -O /usr/bin/del-tr "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/del-tr.sh"
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/cek-tr.sh"
-wget -q -O /usr/bin/trialtrojan "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/trialtrojan.sh"
-wget -q -O /usr/bin/renew-tr "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/renew-tr.sh"
-wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/add-ws.sh"
-wget -q -O /usr/bin/del-ws "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/del-ws.sh"
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/cek-ws.sh"
-wget -q -O /usr/bin/renew-ws "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/renew-ws.sh"
-wget -q -O /usr/bin/trialvmess "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/trialvmess.sh"
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/add-vless.sh"
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/del-vless.sh"
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/cek-vless.sh"
-wget -q -O /usr/bin/renew-vless "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/renew-vless.sh"
-wget -q -O /usr/bin/trialvless "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/xray/trialvless.sh"
-wget -q -O /usr/bin/menu-trial "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/menu/menu-trial.sh"
-wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/theme/menu-theme.sh"
-chmod +x /usr/bin/usernew
-chmod +x /usr/bin/auto-reboot
-chmod +x /usr/bin/restart
-chmod +x /usr/bin/tendang
-chmod +x /usr/bin/clearcache
-chmod +x /usr/bin/running
-chmod +x /usr/bin/speedtest
-chmod +x /usr/bin/menu-vless
-chmod +x /usr/bin/menu-vmess
-chmod +x /usr/bin/menu-theme
-chmod +x /usr/bin/menu1
-chmod +x /usr/bin/menu2
-chmod +x /usr/bin/menu3
-chmod +x /usr/bin/menu4
-chmod +x /usr/bin/menu5
-chmod +x /usr/bin/menu-trojan
-chmod +x /usr/bin/menu-ssh
-chmod +x /usr/bin/menu-backup
-chmod +x /usr/bin/menu
-chmod +x /usr/bin/menu-webmin
-chmod +x /usr/bin/xp
-chmod +x /usr/bin/update
-chmod +x /usr/bin/add-host
-chmod +x /usr/bin/certv2ray
-chmod +x /usr/bin/menu-set
-chmod +x /usr/bin/about
-chmod +x /usr/bin/add4
-chmod +x /usr/bin/menu-backup
-chmod +x /usr/bin/trial
-chmod +x /usr/bin/usernew
-chmod +x /usr/bin/add-tr
-chmod +x /usr/bin/del-tr
-chmod +x /usr/bin/cek-tr
-chmod +x /usr/bin/trialtrojan
-chmod +x /usr/bin/renew-tr
-chmod +x /usr/bin/add-ws
-chmod +x /usr/bin/del-ws
-chmod +x /usr/bin/cek-ws
-chmod +x /usr/bin/renew-ws
-chmod +x /usr/bin/trialvmess
-chmod +x /usr/bin/add-vless
-chmod +x /usr/bin/del-vless
-chmod +x /usr/bin/cek-vless
-chmod +x /usr/bin/renew-vless
-chmod +x /usr/bin/trialvless
-chmod +x /usr/bin/menu-trial
+echo -e "\e[0;32mDownloading new version scripts...\e[0m"
+
+# Download scripts with error handling
+cd /usr/bin || exit
+
+files=(
+  "usernew" "auto-reboot" "restart" "tendang" "clearcache" "running" "speedtest"
+  "menu-vless" "menu-vmess" "menu-trojan" "menu-ssh" "menu-backup" "menu" 
+  "menu1" "menu2" "menu3" "menu4" "menu5" "menu-webmin" "xp" "update" 
+  "add-host" "certv2ray" "menu-set" "about" "trial" "add-tr" "del-tr" 
+  "cek-tr" "trialtrojan" "renew-tr" "add-ws" "del-ws" "cek-ws" "renew-ws" 
+  "trialvmess" "add-vless" "del-vless" "cek-vless" "renew-vless" "trialvless"
+  "menu-trial" "menu-theme"
+)
+
+for file in "${files[@]}"; do
+  wget -q -O "/usr/bin/$file" "https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/${file}.sh" && chmod +x "/usr/bin/$file"
+  if [ $? -ne 0 ]; then
+    echo -e "\e[0;31mFailed to download or set permissions for $file\e[0m"
+  fi
+done
+
 clear
-echo -e "\e[0;32mBerhasil Mendownload Versi Script....!!!\e[0m"
-echo ""
-ver=$( curl https://raw.githubusercontent.com/Lite-VPN/premiumsc/main/version )
-sleep 1
-echo ""
-echo -e "\e[0;32mBerhasil Update Script Ke Versi Terbaru\e[0m"
-cd
-echo "$ver" > /home/ver
+echo -e "\e[0;32mScripts updated successfully....!!!\e[0m"
+
+# Update version
+echo "$new_version" > /home/ver
+
+# Cleanup
 rm -f update.sh
 clear
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Press any key to return to the menu"
 menu
-;;
-esac
