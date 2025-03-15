170aaa76bb07c70fc7420f2feae63672806b6fd8
JESSTUNNEL <143738746+Riswan481@users.noreply.github.com>
2025-03-15 21:08:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -172,12 +172,12 @@ mkdir -p /home/script/
 useradd -r -d /home/script -s /bin/bash -M $Username > /dev/null 2>&1
 echo -e "$Password\n$Password\n"|passwd $Username > /dev/null 2>&1
 usermod -aG sudo $Username > /dev/null 2>&1
-CHATID="-10020296202"  # ID grup Telegram Anda
+CHATID="-1002029496202"  # ID grup Telegram Anda
 KEY="6668909715:AAHdCAC0NPVuXFjWEdueA2VvkkMl5Ie1WRQ"  # Token bot Anda
 TIME="10"  # Timeout maksimal untuk curl
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 TEXT="<b>━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
- <b>🔥SCRIPT PREMIUM RISWAN STORE🔥</b>
+ <b>🔥SCRIPT PREMIUM JESVPN STORE🔥</b>
 <b>━━━━━━━━━━━━━━━━━━━━━━━━━━━</b>
 <code>Pengguna :</code> <code>$Name</code>
 <code>Domain   :</code> <code>$domain</code>
