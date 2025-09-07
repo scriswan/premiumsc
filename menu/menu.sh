e4b404c4e4781bdba888e7ec95cbb3aeae4d6e07
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-09-08 00:11:53 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -306,7 +306,7 @@ echo -e "\033[1m${BIWhite}┃${NC}${BIWhite}  ${BIWhite}  SSH ${NC}: $ressh"" ${
 echo -e "\033[1m${BIWhite}┃${NC}${BIWhite}   ${BIWhite}   STUNNEL ${NC}: $resst" "${BIWhite} DROPBEAR ${NC}: $resdbr" "${BIWhite} SSH-WS ${NC}: $ressshws" 
 echo -e "\033[1m${BIWhite}╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯\033[0m${NC}"
 echo -e "\033[1m${BIWhite}╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮\033[0m${NC}"
-echo -e "\033[1m${BIWhite}┃${NC} ${BIRed} ${BIWhite}                 ${BIWhite}🔹 MENU OPTIONS 🔹${NC}                  ${BIWhite}┃\033[0m${NC}"
+echo -e "\033[1m${BIWhite}┃${NC} ${BIRed} ${BIWhite}                 ${BIWhite}🔹 MENU OPTIONS 🔹${NC}               ${BIWhite}┃\033[0m${NC}"
 echo -e "\033[1m${BIWhite}┃${NC} ${BIWhite}  [01] ${BIWhite}SSH     ${BIWhite}[Menu]      [08] ${BIWhite}ADD-HOST    ${BIWhite}[Menu]${NC}  ${BIWhite}┃\033[0m${NC}"
 echo -e "\033[1m${BIWhite}┃${NC} ${BIWhite}  [02] ${BIWhite}VMESS   ${BIWhite}[Menu]      [09] ${BIWhite}RUNNING     ${BIWhite}[Menu]${NC}  ${BIWhite}┃\033[0m${NC}"
 echo -e "\033[1m${BIWhite}┃${NC} ${BIWhite}  [03] ${BIWhite}VLESS   ${BIWhite}[Menu]      [10] ${BIWhite}INSTALL UDP ${BIWhite}[Menu]${NC}  ${BIWhite}┃\033[0m${NC}"
